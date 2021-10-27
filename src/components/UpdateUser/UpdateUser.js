330a4154dcfefc221ad57a14e3d29b7e78209b31
unknown <siamur01@gmail.com>
2021-10-28 00:52:16 +0600

    64-5-1 update

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+// import { useRef } from 'react';
 
 const UpdateUser = () => {
    const { id } = useParams();
@@ -12,10 +13,46 @@ const UpdateUser = () => {
          .then(data => setUser(data));
 
    })
+
+
+
+   // const nameRef = useRef();
+   // const emailRef = useRef();
+
+   const handleAddUsers = e => {
+
+
+      // const name = nameRef.current.value;
+      // const email = emailRef.current.value;
+
+      // const newUser = { name: name, email: email }
+
+      // fetch('http://localhost:5000/users', {
+      //    method: 'POST',
+      //    headers: {
+      //       'content-type': 'application/json'
+      //    },
+      //    body: JSON.stringify(newUser)
+      // })//feth ses holo
+      //    .then()
+
+
+      // e.preventDefault();
+   }
+
    return (
       <div>
-         <h2>This is Update User : {user.name}</h2>
+         <h2>User : {user.name}</h2>
+         <h2>user email : {user.email}</h2>
          <p>{id}</p>
+
+         <form onSubmit={handleAddUsers}>
+            {/* <input type="text" ref={nameRef} value={user.name} />
+            <input type="email" ref={emailRef} value={user.email} /> */}
+            <input type="text" value={user.name} />
+            <input type="email" value={user.email} />
+            <input type="submit" value="Add" />
+         </form>
       </div>
    );
 };
