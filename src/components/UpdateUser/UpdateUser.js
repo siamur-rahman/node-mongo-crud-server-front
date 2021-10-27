2bdbd471ba73acd2f7942c66291741afa1e9580f
unknown <siamur01@gmail.com>
2021-10-28 00:30:23 +0600

    64-9 end

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const UpdateUser = () => {
    const { id } = useParams();
+   const [user, setUser] = useState({});
 
+   useEffect(() => {
+      const url = `http://localhost:5000/users/${id}`;
+      fetch(url)
+         .then(res => res.json())
+         .then(data => setUser(data));
+
+   })
    return (
       <div>
-         <h2>This is Update User</h2>
+         <h2>This is Update User : {user.name}</h2>
          <p>{id}</p>
       </div>
    );
